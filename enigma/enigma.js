64ff2134059e2d17c40857627ede39f9cf180b96
stwissel <stw@linux.com>
2018-01-17 05:05:05 +0200

    Read keys from config and msg

--- enigma/enigma.js
+++ enigma/enigma.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 St. Wissel, JackyB
+ * Copyright 2015, 2018 St. Wissel, JackyB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,29 +35,39 @@ module.exports = function(RED) {
         node.ReflectorB = 1;
         node.ReflectorC = 2;
 
-        //TODO: Read charmaps from config
-        node.rotorCharMap = [
-            "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
-            "AJDKSIRUXBLHWTMCQGZNPYFVOE",
-            "BDFHJLCPRTXVZNYEIWGAKMUSQO"
-        ];
-
-        node.reflectorCharMap = [
-            "EJMZALYXVBWFCRQUONTSPIKHGD",
-            "YRUHQSLDPXNGOKMIEBFZCWVJAT",
-            "FVPJIAOYEDRZXWGCTKUQSBNMHL"
-        ];
-
-        node.ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            node.NUM_ALPHABETS = 26;
+        node.rotorCharMap = [];
+        node.rotorCharMap.push(n.rotary1);
+        node.rotorCharMap.push(n.rotary2);
+        node.rotorCharMap.push(n.rotary3);
+        
+        node.reflectorCharMap = [];
+        node.reflectorCharMap.push(n.reflector1);
+        node.reflectorCharMap.push(n.reflector2);
+        node.reflectorCharMap.push(n.reflector3);
+        
+        // Deviation from original Enigma. Added dot and space
+        // for better readablility of messages. Make the CPU
+        // sweat a little more for its money 
+        node.ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ .";
+        node.NUM_ALPHABETS = node.ALPHABETS.length;
+        
+        node.encryptionSettings = {
+            notch : node.notch,
+            rotorCharMap : node.rotorCharMap,
+            reflectorCharMap : node.reflectorCharMap,
+            charMap : node.ALPHABETS,
+            NUM_ALPHABETS : node.ALPHABETS.length
+        };
 
         this.Plugboard = function(charMap) {
-            charMap = typeof charMap == "undefined" ? node.ALPHABETS : charMap;
+            charMap = charMap || node.ALPHABETS;
             return {
                 charMap: charMap,
                 processChar: function(ci, reflecting) {
                     var c = this.charMap[ci];
                     return {
+                        // TODO: Should that be??
+                        // idx: this.charMap.indexOf(c)
                         idx: node.ALPHABETS.indexOf(c),
                         reflecting: reflecting
                     };
@@ -68,54 +78,55 @@ module.exports = function(RED) {
             };
         };
 
-        this.Rotor = function(type) {
-            type = typeof type == "undefined" ? node.RotorI : type;
+        this.Rotor = function(type, encryptionSettings) {
+            type = type || node.RotorI;
+            encryptionSettings = encryptionSettings || node.encryptionSettings;
             return {
                 type: type,
                 offset: 0,
                 processChar: function(ci, reflecting) {
                     if (reflecting) {
-                        var idx = (ci + this.offset) % node.NUM_ALPHABETS;
-                        var lc = node.ALPHABETS[idx];
-                        var ri = node.rotorCharMap[this.type].indexOf(lc);
+                        var idx = (ci + this.offset) % encryptionSettings.NUM_ALPHABETS;
+                        var lc = encryptionSettings.charMap[idx];
+                        var ri = encryptionSettings.rotorCharMap[this.type].indexOf(lc);
                         ri -= this.offset;
                         if (ri < 0) {
-                            ri += node.NUM_ALPHABETS;
+                            ri += encryptionSettings.NUM_ALPHABETS;
                         } else {
-                            ri %= node.NUM_ALPHABETS;
+                            ri %= encryptionSettings.NUM_ALPHABETS;
                         }
                         return {
                             idx: ri,
                             reflecting: reflecting
                         };
                     } else {
-                        var idx = (ci + this.offset) % node.NUM_ALPHABETS;
-                        var rc = node.rotorCharMap[this.type][idx];
-                        var li = node.ALPHABETS.indexOf(rc);
+                        var idx2 = (ci + this.offset) % encryptionSettings.NUM_ALPHABETS;
+                        var rc = encryptionSettings.rotorCharMap[this.type][idx2];
+                        var li = encryptionSettings.charMap.indexOf(rc);
                         li -= this.offset;
                         if (li < 0) {
-                            li += node.NUM_ALPHABETS;
+                            li += encryptionSettings.NUM_ALPHABETS;
                         } else {
-                            li %= node.NUM_ALPHABETS;
+                            li %= encryptionSettings.NUM_ALPHABETS;
                         }
                         return {
-                            idx: li,
+                            idx2: li,
                             reflecting: reflecting
                         };
                     }
                 },
                 step: function(n) {
                     var revs = this.countNotchRevs(n);
-                    this.offset = (this.offset + n) % node.NUM_ALPHABETS;
+                    this.offset = (this.offset + n) % encryptionSettings.NUM_ALPHABETS;
                     return revs;
                 },
                 countNotchRevs: function(steps) {
                     // Return 0 if it is determined that the notch won't be reached in the steps
-                    var nch = node.notch[this.type].charCodeAt(0) - 65;
-                    if (this.offset > nch && steps < nch + (node.NUM_ALPHABETS - nch)) {
+                    var nch = encryptionSettings.notch[this.type].charCodeAt(0) - 65;
+                    if (this.offset > nch && steps < nch + (encryptionSettings.NUM_ALPHABETS - nch)) {
                         return 0;
                     }
-                    return this.reallyCountNotchRevs(nch, node.NUM_ALPHABETS, steps);
+                    return this.reallyCountNotchRevs(nch, encryptionSettings.NUM_ALPHABETS, steps);
                 },
                 reallyCountNotchRevs: function(notch, max, steps) {
                     var revs = 0;
@@ -129,25 +140,27 @@ module.exports = function(RED) {
             };
         };
 
-        this.StandardEnigma = function() {
+        this.StandardEnigma = function(encryptionSettings) {
             return new node.EnigmaCore([
-                new node.Plugboard(),
-                new node.Rotor(node.RotorIII),
-                new node.Rotor(node.RotorII),
-                new node.Rotor(node.RotorI),
-                new node.Reflector(node.ReflectorB),
-            ]);
+                new node.Plugboard(encryptionSettings.charMap),
+                new node.Rotor(node.RotorIII, encryptionSettings),
+                new node.Rotor(node.RotorII, encryptionSettings),
+                new node.Rotor(node.RotorI, encryptionSettings),
+                // TODO: make initial reflector configurable
+                new node.Reflector(node.ReflectorB, encryptionSettings)
+            ], encryptionSettings);
         };
 
-        this.Reflector = function(type) {
-            type = typeof type == "undefined" ? node.ReflectorA : type;
+        this.Reflector = function(type, encryptionSettings) {
+            type = type || node.ReflectorA;
+            encryptionSettings = encryptionSettings || node.encryptionSettings;
             return {
                 type: type,
                 charMap: node.reflectorCharMap[type],
                 processChar: function(ci, reflecting) {
-                    var c = node.reflectorCharMap[this.type][ci];
+                    var c = encryptionSettings.reflectorCharMap[this.type][ci];
                     return {
-                        idx: node.ALPHABETS.indexOf(c),
+                        idx: encryptionSettings.charMap.indexOf(c),
                         reflecting: !reflecting
                     };
                 },
@@ -157,8 +170,10 @@ module.exports = function(RED) {
             };
         };
 
-        this.EnigmaCore = function(comps) {
-            comps = typeof comps == "undefined" ? [] : comps;
+        this.EnigmaCore = function(comps, encryptionSettings) {
+            comps = comps || [];
+            encryptionSettings = encryptionSettings || node.encryptionSettings;
+            
             return {
                 components: comps,
                 connect: function(comps) {
@@ -191,9 +206,9 @@ module.exports = function(RED) {
                     }
 
                     if (reflecting) {
-                        for (var i = this.components.length - 2; i >= 0; i--) {
-                            var ret = this.components[i].processChar(ci, reflecting);
-                            ci = ret.idx;
+                        for (var j = this.components.length - 2; i >= 0; i--) {
+                            var ret2 = this.components[j].processChar(ci, reflecting);
+                            ci = ret2.idx;
                         }
                     }
 
@@ -205,13 +220,14 @@ module.exports = function(RED) {
                     }
 
                     for (var comp of this.components) {
-                        steps = comp.step(steps)
+                        steps = comp.step(steps);
                         if (steps <= 0) {
                             break;
                         }
                     }
                 },
                 sanitize: function(s) {
+                    //TODO allow all chars in charmap!
                     s = s.trim();
                     s = s.toUpperCase();
                     for (var i = 0; i < s.length; i++) {
@@ -224,17 +240,28 @@ module.exports = function(RED) {
                     return s;
                 },
             };
-        }
+        };
 
         // Run the encryption / decryption process on the payload
         this.on('input', function(msg) {
             var raw = msg[node.field] || msg.payload;
             var logobject = {};
             logobject.messages = [];
+            
+            // Check the message object for key overwrites
+            // msg.rotary msg.reflector msg.notch
+            const encryptionSettings = {};
+            // No check is done if the msg properties are in the
+            // right format. The node will barf if not
+            encryptionSettings.notch = msg.notch || node.notch;
+            encryptionSettings.rotorCharMap = msg.rotary || node.rotorCharMap;
+            encryptionSettings.reflectorCharMap = msg.reflector || node.reflectorCharMap;
+            encryptionSettings.charMap = msg.charMap || node.ALPHABETS;
+            encryptionSettings.NUM_ALPHABETS =  encryptionSettings.charMap.length;
 
             try {
 
-                var curEnigma = node.StandardEnigma();
+                var curEnigma = node.StandardEnigma(encryptionSettings);
                 var result = curEnigma.encrypt(raw);
                 if (node.field) {
                     msg[node.field] = result.encrypted;
@@ -254,7 +281,7 @@ module.exports = function(RED) {
                     shape: "dot",
                     text: err.message
                 });
-                node.error(err.message);
+                node.error(err, err.message);
             }
         });
 
@@ -266,4 +293,4 @@ module.exports = function(RED) {
     }
     // Make it known to the UI
     RED.nodes.registerType("enigma", Enigma);
-}
+};
